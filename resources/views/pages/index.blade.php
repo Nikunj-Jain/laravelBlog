522039bbd0c91f57977c4da38c6ac73931fc808f
Nikunj Jain <nj.nikunjjain@gmail.com>
2017-11-11 22:55:39 +0530

    End of Video 5 commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>Welcome to Laravel!</h1>
-        <p>THis is the laravel application that I've built</p>
+        <p>This is the laravel application that I've built</p>
         <p><a class="btn btn-primary btn-lg" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a> </p>
     </div>
 @endsection
